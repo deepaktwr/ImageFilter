b38b1817061a9552b93a6e6bcd5b8f3855b7262f
deepaktwr <deepaktwr020@gmail.com>
2015-07-30 07:27:55 +0530

    static things

--- app/src/main/java/proj/android/helper/Utils.java
+++ app/src/main/java/proj/android/helper/Utils.java
@@ -4,15 +4,43 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
+import proj.android.gl_helper.GlSurfaceView;
+
 /**
  * Created by deepak on 24/5/15.
  */
 public class Utils {
     private static final String TAG = "FILTER";
+    private static boolean isProgramReady = false;
     public static void logMessage(String message){
+        Log.i(TAG, message);
+    }
+    public static void logError(String message){
         Log.e(TAG, message);
     }
     public static void showToast(Context context,String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+    public float c(float val){
+        return val/100f;
+    }
+    public static String formatMessage(String message, Object ... values){
+        return String.format(message, values);
+    }
+
+    public static boolean isProgramReady() {
+        return isProgramReady;
+    }
+
+    public static void setIsProgramReady(boolean isProgramReady) {
+        Utils.isProgramReady = isProgramReady;
+    }
+
+    private static GlSurfaceView glSurfaceView;
+    public static void setSurfaceViewObj(GlSurfaceView glSurfaceView1){
+        glSurfaceView=glSurfaceView1;
+    }
+    public static GlSurfaceView getSurfaceView(){
+        return glSurfaceView;
     }
 }
