eebb0da70adb6b3889f179facc7033ed1c6f3198
deepaktwr <deepaktwr020@gmail.com>
2015-07-30 07:32:01 +0530

    renderer for demos

--- app/src/main/java/proj/android/gl_helper/GlRenderer.java
+++ app/src/main/java/proj/android/gl_helper/GlRenderer.java
@@ -1,51 +1,90 @@
 package proj.android.gl_helper;
 
 import android.content.Context;
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
-
-import proj.android.gl_shapes.ShapeSquare;
-import proj.android.gl_shapes.ShapeTriangle;
+import proj.android.gl_shapes.DemoShape;
+import proj.android.imagefilter.R;
 
 /**
- * Created by deepak on 11/6/15.
+ * the class basically help to render the shapes on the opengl window.
+ * code is influenced from <a href="http://alfonse.bitbucket.org/oldtut/">this tutorial</a>
+ * and <a href ="https://www.khronos.org/opengles/sdk/docs/man3/">openGL docs</a>
+ * @author deepak
  */
 public class GlRenderer implements GLSurfaceView.Renderer{
 
-    private ShapeTriangle shapeTriangle;
-    //private ShapeSquare shapeSquare;
     private Context context;
+    private DemoShape demoShape;
 
     public GlRenderer(Context context){
         super();
         this.context=context;
     }
+    /**
+     * color will be used when window will be cleared; only need to set once
+     * */
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        GLES20.glClearColor(0.09f, 0.08f, 0.01f, 1.0f);
+        //for depth
+        //GlDecorator.setClearingDepthOfWindow();
 
-        shapeTriangle=ShapeTriangle.getInstance(context);
-        //shapeSquare=ShapeSquare.getInstance();
+        GlDecorator.setClearingColorOfWindow();
+
+        //for texture
+        GlDecorator.loadTextureFromResource(context, R.mipmap.ic_launcher);
+
+        /*String version =  gl10.glGetString(GL10.GL_VERSION);
+        Log.w("vvr", "Version: " + version);
+
+        String versions =  gl10.glGetString(GLES30.GL_SHADING_LANGUAGE_VERSION);
+        Log.w("vvr", "SHADING LANGUAGE: " + versions);
+
+        Utils.logMessage(gl10.glGetString(GLES30.GL_VENDOR));
+        Utils.logMessage(gl10.glGetString(GLES30.GL_VERSION));
+        Utils.logMessage(gl10.glGetString(GLES30.GL_SHADING_LANGUAGE_VERSION));*/
+
+
+        initializeShapes();
     }
-
+    /**
+     * set the view port of the opengl screen;
+     * opengl has view port like bottomLeft X and Y cord and width and height to the right*/
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
-    }
+        GlDecorator.initializeViewPort(0, 0, width, height);
 
+        GlDecorator.setViewPort();
+
+    }
+    /**
+     * clearing the window and applying the color on that window which is specified by glClearColor;
+     * the default buffer which will be cleared will be COLOR_BUFFER;
+     * @see GlDecorator
+     * */
     @Override
     public void onDrawFrame(GL10 gl10) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        shapeTriangle.drawTriangleShape();
+        //for depth
+        //GlDecorator.intializeClearBuffers(GLES30.GL_COLOR_BUFFER_BIT | GLES30.GL_DEPTH_BUFFER_BIT);
+
+        GlDecorator.clearWindowWithBuffers();
+
+        //demoShape.draw2DShapes();
+        //demoShape.drawColored2DShapes();
+        //demoShape.drawMovingShapes();
+        //demoShape.drawUsingUniform();
+        //demoShape.drawPerspective();
+        //demoShape.drawPerspectiveNew();
+        //demoShape.drawSimpleIndexDraw();
+        //demoShape.drawIndexedDrawing();
+        //demoShape.drawOptimizedElementDraw();textureCoords
+
+
+        demoShape.drawTexture();
     }
 
-    public static int loadShader(int shaderType, String shaderCode){
-        int shader = GLES20.glCreateShader(shaderType);
-        GLES20.glShaderSource(shader, shaderCode);
-        GLES20.glCompileShader(shader);
-        return shader;
+    private final void initializeShapes(){
+        demoShape = DemoShape.getIntance(context);
     }
 }
